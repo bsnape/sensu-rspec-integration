76bc4557033434cbe0c0515648a37da24a65d803
Ben Snape <ben.snape@itv.com>
2015-06-18 12:27:39 +0100

    improved handler logic

--- modules/data/files/plugins/check-rspec.rb
+++ modules/data/files/plugins/check-rspec.rb
@@ -54,24 +54,35 @@ class CheckRspec < Sensu::Plugin::Check::CLI
          :long     => '--env-var ENVIRONMENT=sit',
          :required => false
 
-  option :handler,
-         :short   => '-l HANDLER',
-         :long    => '--handler HANDLER',
+  option :handlers,
+         :short   => '-l HANDLERS',
+         :long    => '--handlers HANDLERS',
          :default => 'default'
 
-  def sensu_client_socket(msg)
-    u = UDPSocket.new
-    u.send(msg + "\n", 0, '127.0.0.1', 3030)
+  OK       = 0
+  WARNING  = 1
+  CRITICAL = 2
+
+  def sensu_client_socket(message)
+    UDPSocket.new.send(message + "\n", 0, '127.0.0.1', 3030)
   end
 
-  def send_ok(check_name, msg)
-    d = { 'name' => check_name, 'status' => 0, 'output' => "OK: #{msg}", 'handler' => config[:handler] }
-    sensu_client_socket d.to_json
+  def send_ok(alert_name, alert_message)
+    handlers     = config[:handlers].split(',').map(&:strip)
+    check_result = { :name => alert_name, :status => OK, :output => "OK: #{alert_message}", :handlers => handlers }
+    sensu_client_socket check_result.to_json
   end
 
-  def send_warning(check_name, msg)
-    d = { 'name' => check_name, 'status' => 1, 'output' => "WARNING: #{msg}", 'handler' => config[:handler] }
-    sensu_client_socket d.to_json
+  def send_warning(alert_name, alert_message)
+    handlers     = config[:handlers].split(',').map(&:strip)
+    check_result = { :name => alert_name, :status => WARNING, :output => "WARNING: #{alert_message}", :handlers => handlers }
+    sensu_client_socket check_result.to_json
+  end
+
+  def send_critical(alert_name, alert_message)
+    handlers     = config[:handlers].split(',').map(&:strip)
+    check_result = { :name => alert_name, :status => CRITICAL, :output => "CRITICAL: #{alert_message}", :handlers => handlers }
+    sensu_client_socket check_result.to_json
   end
 
   def run
